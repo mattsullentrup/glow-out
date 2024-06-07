b3f5cef8772fab8f7eded7d355f78b87527365a4
mattsullentrup <msullentrup1@gmail.com>
2024-06-07 16:36:23 -0500

    Replace state machine with component based system

--- mini_jam_160_platformer/scripts/player.gd
+++ mini_jam_160_platformer/scripts/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@export_subgroup("Nodes")
+@export var gravity_component: GravityComponent
+@export var input_component: InputComponent
+@export var movement_component: MovementComponent
+@export var animation_component: AnimationComponent
+@export var jump_component: JumpComponent
 
 
-func _process(_delta: float) -> void:
-	if velocity.x > 0:
-		sprite.flip_h = false
-	elif velocity.x < 0:
-		sprite.flip_h = true
+func _physics_process(delta: float) -> void:
+	gravity_component.handle_gravity(self, delta)
+	movement_component.handle_horizontal_movement(self, input_component.input_horizontal)
+	animation_component.handle_move_animation(input_component.input_horizontal)
+	jump_component.handle_jump(self, input_component.get_jump_input())
+	animation_component.handle_jump_animation(jump_component.is_jumping, gravity_component.is_falling)
+
+	move_and_slide()
