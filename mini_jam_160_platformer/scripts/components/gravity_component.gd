5d4c2c8d33e099e98d038291023ce10db5ed4eec
mattsullentrup <msullentrup1@gmail.com>
2024-06-09 14:45:39 -0500

    Set max player fall speed

--- mini_jam_160_platformer/scripts/components/gravity_component.gd
+++ mini_jam_160_platformer/scripts/components/gravity_component.gd
@@ -4,6 +4,7 @@ extends Node
 
 @export_subgroup("Settings")
 @export var gravity: float = 1000.0
+@export var max_fall_speed: float = 300
 
 var is_falling: bool = false
 
@@ -13,3 +14,6 @@ func handle_gravity(body: CharacterBody2D, delta: float) -> void:
 		body.velocity.y += gravity * delta
 
 	is_falling = body.velocity.y > 0 and not body.is_on_floor()
+
+	if is_falling:
+		body.velocity.y = clampf(body.velocity.y, 0, max_fall_speed)
