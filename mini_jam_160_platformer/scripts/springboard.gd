6b79e7b38282eb4e5437db9d50afd3e8de843a9d
mattsullentrup <msullentrup1@gmail.com>
2024-06-12 20:25:34 -0500

    Add diagonal directions to springboard

--- mini_jam_160_platformer/scripts/springboard.gd
+++ mini_jam_160_platformer/scripts/springboard.gd
@@ -1,12 +1,33 @@
 extends Area2D
 
 
-@export var spring_direction: Globals.Directions
+var direction_pairs: Dictionary = {
+	0: Vector2.UP,
+	45: Vector2(1, -1),
+	90: Vector2.RIGHT,
+	135: Vector2.ONE,
+	180: Vector2.DOWN,
+	225: Vector2(-1, 1),
+	270: Vector2.LEFT,
+	315: -Vector2.ONE
+}
+
 @export var force: float = 500
+
+var spring_direction: Vector2
 
 
 func _ready() -> void:
+	initialize_directions()
 	$AnimatedSprite2D.frame = 0
+
+
+func initialize_directions() -> void:
+	var rot = roundi(rotation_degrees)
+	while rot < 0:
+		rot += 360
+	var dir = direction_pairs.get(rot)
+	spring_direction = dir
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -14,20 +35,21 @@ func _on_body_entered(body: Node2D) -> void:
 		return
 
 	var player = body as Player
-	#var force: Vector2 = spring_direction * force
 	match spring_direction:
-		Globals.Directions.NORTH when player.is_on_floor():
+		Vector2.UP when player.is_on_floor():
 			var jump_component = player.get_node("AdvancedJumpComponent") as AdvancedJumpComponent
 			jump_component.is_jumping = true
 			player.velocity.y = -force
-		Globals.Directions.NORTH:
+		Vector2.UP:
 			player.velocity.y = -force
-		Globals.Directions.SOUTH:
+		Vector2.DOWN:
 			player.velocity.y = force
-		Globals.Directions.EAST:
+		Vector2.RIGHT:
 			player.velocity.x = force
-		Globals.Directions.WEST:
+		Vector2.LEFT:
 			player.velocity.x = -force
 		_:
-			printerr("Invalid direction")
+			player.velocity = spring_direction * force
+			#printerr("Invalid direction")
+
 	$AnimationPlayer.play("spring")
