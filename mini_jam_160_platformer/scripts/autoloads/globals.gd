e4f9a6b4655122708a5a945a319dbc910a3b95ff
mattsullentrup <msullentrup1@gmail.com>
2024-06-19 16:07:16 -0500

    Change scene transitions

--- mini_jam_160_platformer/scripts/autoloads/globals.gd
+++ mini_jam_160_platformer/scripts/autoloads/globals.gd
@@ -1,21 +1,32 @@
 extends Node
 
 
-#var levels: Array[PackedScene] = []
+const TRANSITION_COLOR_RECT = preload("res://scenes/transition_color_rect.tscn")
+
 var levels: Array[String] = []
 
 
 func play_exit_transition(scene: Node) -> void:
+	var transition_scene = TRANSITION_COLOR_RECT.instantiate()
+	var color_rect = transition_scene.get_child(0)
+	color_rect.color = Color(Color.BLACK, 0)
+	scene.add_child(transition_scene)
+
 	var tween: PropertyTweener = create_tween().tween_property(
-			scene, "modulate", Color.BLACK, 1)
+			color_rect, "color", Color.BLACK, 1)
 	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	await tween.finished
 	scene.queue_free()
 
 
 func play_enter_transition(scene: Node) -> void:
+	var transition_scene = TRANSITION_COLOR_RECT.instantiate()
+	var color_rect = transition_scene.get_child(0)
+	color_rect.color = Color(Color.BLACK, 1)
+	scene.add_child(transition_scene)
+
 	var tween: Tween = create_tween()
-	tween.tween_property(scene, "modulate", Color.WHITE, 1).from(Color.BLACK)
+	tween.tween_property(color_rect, "color", Color(Color.BLACK, 0), 1)
 	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	await tween.finished
-	#scene.queue_free()
+	transition_scene.queue_free()
