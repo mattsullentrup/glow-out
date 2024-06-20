a35533af48e7ff66c50056c00249579042e7fd43
mattsullentrup <msullentrup1@gmail.com>
2024-06-19 19:04:06 -0500

    Refactor loading system

--- mini_jam_160_platformer/scripts/autoloads/scene_loader.gd
+++ mini_jam_160_platformer/scripts/autoloads/scene_loader.gd
@@ -1,36 +1,46 @@
 extends Node
 
 
+signal progress_changed(progress: float)
+
 const LOADING_SCREEN = preload("res://scenes/loading_screen.tscn")
 
-
 var scene_path: String
-var is_loading := false
+#var is_loading := false
+var progress: Array = []
 
 
 func _process(_delta: float) -> void:
-	if not is_loading:
-		return
+	#if not is_loading:
+		#return
 
-	var progress: Array = []
+	check_load_status()
+
+
+func check_load_status() -> void:
 	var status: ResourceLoader.ThreadLoadStatus = ResourceLoader.load_threaded_get_status(
 			scene_path, progress)
 
-	if status == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
-		create_loading_screen(progress)
-	elif status == ResourceLoader.THREAD_LOAD_LOADED:
-		var new_scene: PackedScene = ResourceLoader.load_threaded_get(scene_path)
-		get_tree().change_scene_to_packed(new_scene)
-		is_loading = false
-	else:
-		printerr("loading fucked up")
+	match status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			progress_changed.emit(progress[0])
+		ResourceLoader.THREAD_LOAD_LOADED:
+			var new_scene: PackedScene = ResourceLoader.load_threaded_get(scene_path)
+			progress_changed.emit(1.0)
+			get_tree().change_scene_to_packed(new_scene)
+		ResourceLoader.THREAD_LOAD_FAILED:
+			printerr("loading fucked up")
+		ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
+			print("not loading")
+			set_process(false)
 
 
-func create_loading_screen(progress: Array) -> void:
+func create_loading_screen() -> void:
 	var loading_screen: LoadingScreen = LOADING_SCREEN.instantiate()
 	if loading_screen is LoadingScreen:
 		get_tree().root.call_deferred("add_child", loading_screen)
-		loading_screen.progress = progress
+		progress_changed.connect(loading_screen.update_progress_bar)
+		#loading_screen.progress = progress
 
 
 func load_scene(path: String) -> void:
@@ -39,9 +49,9 @@ func load_scene(path: String) -> void:
 	else:
 		ResourceLoader.load_threaded_request(path)
 
+	create_loading_screen()
+	set_process(true)
+	scene_path = path
 	var current_scene: Node = get_tree().current_scene
 	Globals.play_exit_transition(current_scene)
 	await current_scene.tree_exited
-
-	is_loading = true
-	scene_path = path
--- mini_jam_160_platformer/scripts/loading_screen.gd
+++ mini_jam_160_platformer/scripts/loading_screen.gd
@@ -2,8 +2,6 @@ class_name LoadingScreen
 extends Control
 
 
-var progress: Array = []
-
 @onready var progress_bar: ProgressBar = $MarginContainer/ProgressBar
 
 
@@ -11,11 +9,8 @@ func _ready() -> void:
 	progress_bar.value = 0
 
 
-func _process(_delta: float) -> void:
-	if not progress:
-		return
-
-	progress_bar.value = progress[0] * 100
+func update_progress_bar(new_value: float) -> void:
+	progress_bar.value = new_value * 100
 
 	if progress_bar.value == progress_bar.max_value:
 		queue_free()
