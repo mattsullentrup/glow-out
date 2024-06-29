b80755de09b20140b195c49ad56104d4dd5ee52f
mattsullentrup <msullentrup1@gmail.com>
2024-06-29 17:22:40 -0500

    amend

--- mini_jam_160_platformer/scripts/menus/end_screen.gd
+++ mini_jam_160_platformer/scripts/menus/end_screen.gd
@@ -3,6 +3,12 @@ extends Control
 
 const MAIN_MENU := "res://scenes/menus/main_menu.tscn" as String
 
+@onready var button: Button = $MarginContainer/VBoxContainer/Button
+
+
+func _ready() -> void:
+	button.grab_focus()
+
 
 func _on_button_pressed() -> void:
 	SceneLoader.load_scene(MAIN_MENU)
