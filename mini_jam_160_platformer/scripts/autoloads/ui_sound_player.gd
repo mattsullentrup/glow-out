346b48a1e814d08df2e67ce6f2a6d06b008f0c4e
mattsullentrup <msullentrup1@gmail.com>
2024-06-30 16:59:13 -0500

    Setup ui sounds script

---
+++ mini_jam_160_platformer/scripts/autoloads/ui_sound_player.gd
@@ -0,0 +1,39 @@
+extends Node
+
+
+@onready var click_sound: AudioStreamPlayer = $ClickSound
+@onready var button_sound: AudioStreamPlayer = $ButtonSound
+
+
+func _ready() -> void:
+	# when _ready is called, there might already be nodes in the tree, so connect all existing buttons
+	connect_buttons(get_tree().root)
+	get_tree().node_added.connect(_on_scene_tree_node_added)
+
+
+# recursively connect all buttons
+func connect_buttons(root: Node) -> void:
+	for child in root.get_children():
+		if child is BaseButton:
+			var button: Button = child
+			connect_to_button(button)
+		connect_buttons(child)
+
+
+func connect_to_button(button: BaseButton) -> void:
+	button.pressed.connect(_on_button_pressed)
+	button.focus_exited.connect(_on_button_focus_exited)
+
+
+func _on_scene_tree_node_added(node: Node) -> void:
+	if node is Button:
+		var button: Button = node
+		connect_to_button(button)
+
+
+func _on_button_pressed() -> void:
+	button_sound.play()
+
+
+func _on_button_focus_exited() -> void:
+	click_sound.play()
