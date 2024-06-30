a31cad42399a0f59ef7934833bc6ab5939a3c3d1
mattsullentrup <msullentrup1@gmail.com>
2024-06-30 18:04:06 -0500

    Add key collision sound

--- mini_jam_160_platformer/scripts/gameplay_elements/key.gd
+++ mini_jam_160_platformer/scripts/gameplay_elements/key.gd
@@ -4,10 +4,18 @@ extends Area2D
 
 signal player_found_key
 
+@onready var key_sound: AudioStreamPlayer = $KeySound
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is not Player:
 		return
 
+	key_sound.play()
+	sprite_2d.visible = false
+	collision_shape.set_deferred("disabled", true)
 	player_found_key.emit()
+	await key_sound.finished
 	queue_free()
