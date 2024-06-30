b039c40e61042da59207638f7bd1e37009fb030b
mattsullentrup <msullentrup1@gmail.com>
2024-06-30 15:20:36 -0500

    Add level restart button

--- mini_jam_160_platformer/scripts/components/input_component.gd
+++ mini_jam_160_platformer/scripts/components/input_component.gd
@@ -3,10 +3,26 @@ extends Node
 
 
 var input_horizontal: float = 0.0
+var time_held: float = 0.0
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	input_horizontal = Input.get_axis("left", "right")
+
+	get_restart_input(delta)
+
+
+func get_restart_input(delta: float) -> void:
+	print(time_held)
+	if not Input.is_action_pressed("restart"):
+		time_held = 0
+		return
+
+	time_held += delta
+	if time_held > 1:
+		#player_restarting.emit()
+		time_held = 0
+		get_tree().reload_current_scene()
 
 
 func get_jump_input() -> bool:
